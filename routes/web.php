8d1418814e6ca3cb7db74c2fcb551debbdd7df0e
Chris N <hi.chrisn@gmail.com>
2021-09-23 18:02:21 -0300

    roteamento e controladores p1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,75 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Definição de rota básica
+Route::get('/', function () {
+    return 'Hello, World!';
+});
+
+// Exemplo de site
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('products', function () {
+    return view('products');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+// chamadas estaticas - evitar o uso de facades
+$router->get('/', function () {
+    return 'Hello, World!';
+});
+
+// Verbos de rota
+Route::get('/', function () {
+    return 'Hello, World!';
+});
+
+Route::post('/', function () {});
+Route::put('/', function () {});
+Route::delete('/', function () {});
+Route::any('/', function () {});
+Route::match(['get', 'post'], '/', function () {});
+
+// Rotas chamando métodos de controlador
+Route::get('/', 'WelcomeController@index');
+
+// Parâmetros de rota
+Route::get('users/{id}/friends', function ($id) {
+    // todo
+});
+
+// parâmentros de rota e parâmentros de métodos
+Route::get('users/{userId}/comments/{commentId}', function (
+    $thisIsActuallyTheUserId,
+    $thisIsReallyTheCommentId
+    ) {
+    //
+});
+
+// Parâmetros de rota opcionais
+Route::get('users/{id?}', function ($id = 'fallbackId') {
+    //
+});
+
+// Restrições de rota definidas em expressões regulares
+Route::get('users/{id}', function ($id) {
+    // todo
+})->where('id', '[0-9]+');
+
+Route::get('users/{username}', function ($username) {
+    // todo
+})->where('username', '[A-Za-z]+');
+
+Route::get('posts/{id}/{slug}', function ($id, $slug) {
+    // todo
+})->where(['id' => '[0-9]+', 'slug' => '[A-Za-z]+']);
